49712e4838ad456e7f8f8b79a4d51241ae04fc0c
Semih Cihan <umitsemihcihan@gmail.com>
2023-07-04 18:15:56 +0300

    reset screenshot tests after each

--- MoviesUITests/MoviesScreenshots.swift
+++ MoviesUITests/MoviesScreenshots.swift
@@ -16,16 +16,17 @@ final class MoviesUITests: XCTestCase {
         continueAfterFailure = false
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        
+        Snapshot.app?.launch()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        Snapshot.app?.terminate()
     }
     
     override class func setUp() {
-        let app = XCUIApplication()
-        setupSnapshot(app)
-        app.launch()
+        setupSnapshot(XCUIApplication())
     }
 
     func testScreenshotHome() throws {
@@ -33,21 +34,11 @@ final class MoviesUITests: XCTestCase {
     }
     
     func testScreenshotFilter() throws {
-        
         let app = XCUIApplication()
         let collectionViewsQuery = app.collectionViews
         collectionViewsQuery/*@START_MENU_TOKEN@*/.buttons["7+"]/*[[".cells.buttons[\"7+\"]",".buttons[\"7+\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         collectionViewsQuery.buttons["Movie"].tap()
         
         snapshot("Filter")
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
     }
 }
